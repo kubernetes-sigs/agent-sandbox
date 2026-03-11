5c6bb9368981d15f7af1a56ac4d6648a8064ff67
Aditya Shantanu <adityashantanu@google.com>
2026-03-11 21:37:31 +0000

    header and gofmt

--- test/e2e/parallelism_test.go
+++ test/e2e/parallelism_test.go
@@ -1,3 +1,17 @@
+// Copyright 2026 The Kubernetes Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package e2e
 
 import (
@@ -199,7 +213,7 @@ func TestParallelSandboxClaimsWithSufficientWarmPool(t *testing.T) {
 	poolObj.Spec.Replicas = poolSize
 	poolObj.Spec.TemplateRef.Name = template.Name
 	require.NoError(t, tc.CreateWithCleanup(t.Context(), poolObj))
-	
+
 	require.NoError(t, tc.WaitForWarmPoolReady(t.Context(), types.NamespacedName{Name: poolObj.Name, Namespace: poolObj.Namespace}))
 
 	numClaims := 20
@@ -263,7 +277,7 @@ func TestParallelSandboxClaimsWithInsufficientWarmPool(t *testing.T) {
 	poolObj.Spec.Replicas = poolSize
 	poolObj.Spec.TemplateRef.Name = template.Name
 	require.NoError(t, tc.CreateWithCleanup(t.Context(), poolObj))
-	
+
 	require.NoError(t, tc.WaitForWarmPoolReady(t.Context(), types.NamespacedName{Name: poolObj.Name, Namespace: poolObj.Namespace}))
 
 	numClaims := 20
@@ -296,5 +310,3 @@ func TestParallelSandboxClaimsWithInsufficientWarmPool(t *testing.T) {
 		t.Errorf("Error during parallel run: %v", err)
 	}
 }
-
-

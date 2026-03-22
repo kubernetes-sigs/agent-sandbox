d250295a2a318cb068f8bb44adb3d98fe5aedab0
dongjiang1989 <dongjiang1989@126.com>
2026-03-22 16:05:41 +0800

    fix test e2e
    Signed-off-by: dongjiang1989 <dongjiang1989@126.com>

--- extensions/controllers/sandboxtemplate_controller.go
+++ extensions/controllers/sandboxtemplate_controller.go
@@ -47,6 +47,7 @@ type SandboxTemplateReconciler struct {
 //+kubebuilder:rbac:groups=extensions.agents.x-k8s.io,resources=sandboxtemplates,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=extensions.agents.x-k8s.io,resources=sandboxtemplates/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=networking.k8s.io,resources=networkpolicies,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=networking.k8s.io,resources=networkpolicies/finalizers,verbs=get;update;patch
 
 func (r *SandboxTemplateReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	logger := log.FromContext(ctx)

bb5c2ac0457bf0e5751230e9fe873abd45c1f92f
Vicente Ferrara <vicenteferrara@google.com>
2026-03-30 02:01:59 +0000

    optimization: replace the r.List() block that iterates over all sandboxes with channel pop
    nit
    
    fix
    
    fix
    
    nit
    
    nit
    
    nit
    
    nit
    
    clean up
    
    clean up comments
    
    nit
    
    lint
    
    fix

--- cmd/agent-sandbox-controller/main.go
+++ cmd/agent-sandbox-controller/main.go
@@ -238,6 +238,11 @@ func main() {
 			os.Exit(1)
 		}
 
+		if err := assigner.SetupWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to set up pure-push watcher")
+			os.Exit(1)
+		}
+
 		if err = (&extensionscontrollers.SandboxClaimReconciler{
 			Client:   mgr.GetClient(),
 			Scheme:   mgr.GetScheme(),
@@ -298,9 +303,10 @@ func main() {
 		}
 
 		if isReady {
-			// Return the template hash so we can query specific templates instantly
-			templateHash := sandbox.Labels["agents.x-k8s.io/sandbox-template-ref-hash"]
-			return []string{"true-" + templateHash}
+			templateName := sandbox.Labels["agents.x-k8s.io/sandbox-template-ref"]
+			if templateName != "" {
+				return []string{"true-" + templateName}
+			}
 		}
 		return nil
 	}); err != nil {

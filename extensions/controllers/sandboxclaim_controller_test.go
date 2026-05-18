619b97e4d3477d23bfa8588512ba49bb1ec7df3d
Noel <noel@noel.sh>
2026-05-18 11:38:25 +0200

    test: use ptr helper for owner reference

--- extensions/controllers/sandboxclaim_controller_test.go
+++ extensions/controllers/sandboxclaim_controller_test.go
@@ -2315,7 +2315,7 @@ func TestSandboxClaimWithWorkspaceResourcesSkipsWarmAdoption(t *testing.T) {
 					Kind:       "SandboxWarmPool",
 					Name:       "test-pool",
 					UID:        "pool-uid",
-					Controller: new(true),
+					Controller: ptr.To(true),
 				},
 			},
 		},

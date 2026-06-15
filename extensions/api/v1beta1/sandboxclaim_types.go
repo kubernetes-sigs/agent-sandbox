ff1ba4917e174fe8e1bb98e4c6463df81ea0dd29
Sairaj Pokale <sairajpokale@google.com>
2026-06-15 20:58:55 +0000

    Feat(extensions): add printer columns to SandboxClaim CRD (#963)

--- extensions/api/v1beta1/sandboxclaim_types.go
+++ extensions/api/v1beta1/sandboxclaim_types.go
@@ -153,6 +153,9 @@ type SandboxStatus struct {
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
 // +kubebuilder:resource:scope=Namespaced,shortName=sandboxclaim
+// +kubebuilder:printcolumn:name="Ready",type="string",JSONPath=".status.conditions[?(@.type=='Ready')].status"
+// +kubebuilder:printcolumn:name="Sandbox",type="string",JSONPath=".status.sandbox.name"
+// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
 // SandboxClaim is the Schema for the sandbox Claim API.
 type SandboxClaim struct {
 	metav1.TypeMeta `json:",inline"`

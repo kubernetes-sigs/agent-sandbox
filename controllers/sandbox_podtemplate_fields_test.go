fe1c6434de27fc5afdea872327970543cf11d842
dongjiang1989 <dongjiang1989@126.com>
2026-09-05 16:13:54 +0800

    style: apply gofmt formatting to podTemplate field tests
    Fix import ordering flagged by gofmt linter.

--- controllers/sandbox_podtemplate_fields_test.go
+++ controllers/sandbox_podtemplate_fields_test.go
@@ -21,12 +21,12 @@ import (
 
 	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/validation/field"
-	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/utils/ptr"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
